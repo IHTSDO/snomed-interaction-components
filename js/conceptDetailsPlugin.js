1ea8b0863cb0eb16700ccbe4fd46fb4d6d9272fb
Chris Swires <csw@ihtsdo.org>
2019-09-05 09:30:10 +0100

    BROWSE-180 - updated issue collector to use HTML link with inclusive jQuery embedded in handlebars template

--- js/conceptDetailsPlugin.js
+++ js/conceptDetailsPlugin.js
@@ -704,30 +704,6 @@ function conceptDetails(divElement, conceptId, options) {
                 }
             }
 
-
-            //Swedish extension; capture synonyms using JIRA issue collector
-            //start
-            var scriptUrl = "https://jira.ihtsdotools.org/s/9152b378d577114d19d6cfdcdfdeb45e-T/en_US-i9n6p8/70120/a1623a9e469981bb7c457209f1507980/2.0.8/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=41bec258";
-
-            $.getScript(scriptUrl);
-
-            window.ATL_JQ_PAGE_PROPS = {
-                "triggerFunction": function(showCollectorDialog) {
-                    //Requires that jQuery is available!
-                    jQuery("#fh-cd1_canvas-addsyn-sctid-details").click(function(e) {
-                        e.preventDefault();
-                        showCollectorDialog();
-                    });
-                },
-
-                fieldValues: {
-                    'summary': 'Förslag på synonymer för begreppet: ' + state.conceptId,
-                    'customfield_10602': state.conceptId,
-                    'customfield_10601': state.name
-                }
-            };
-            //end
-
             $(".glyphicon-star").click(function(e) {
                 var concept = {
                     module: firstMatch.module,
